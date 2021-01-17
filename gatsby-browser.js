66e9063c37d383769abf0d34c6495fe35c025dd4
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-01-17 13:41:47 +0100

    feat: remove www

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,13 @@
 import "./src/styles/app.scss"
+
+const ensureCorrectURL = () => {
+  if (window.location.hostname.indexOf('www') === 0) {
+    window.location = window.location.href.replace('www.', '')
+  }
+}
+
+export const onClientEntry = () => {
+  window.onload = () => {
+    ensureCorrectURL()
+  }
+}
