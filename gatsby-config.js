5459ad4909d3054228d9e215d600941c5317f769
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-01-17 17:04:17 +0100

    feat: animations on landing done

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         'gatsby-plugin-sass',
+        'gatsby-plugin-scroll-reveal',
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,7 +87,7 @@ const IndexPage = () => {
       <div className="wrapper">
         {/* Header */}
         <div className="container-fluid">
-          <div className="row middle-xs header">
+          <div className="row middle-xs header" data-sal="slide-down" data-sal-delay="600" data-sal-easing="ease-out-back">
             <div className="col-lg-6 col-md-4 col-xs-2">
               <h1 className="header__logo">
                 BigSeat.
@@ -112,7 +112,7 @@ const IndexPage = () => {
               </div>
             </div>
           </div>
-          <div className="row introduction">
+          <div className="row introduction" data-sal="slide-up" data-sal-delay="200" data-sal-easing="ease-out-back">
             <div className="col-xs-12">
               <div className="row">
                 <div className="col-xs-12">
@@ -172,7 +172,7 @@ const IndexPage = () => {
                   </h2>
                 </div>
               </div>
-              <div className="row features-block middle-xs center-xs">
+              <div className="row features-block middle-xs center-xs" data-sal="slide-right" data-sal-delay="200" data-sal-easing="ease-out-back">
                 <div className="col-xs-12 col-sm-5">
                   <h3 className="features-block__title">Accessibility.</h3>
                   <p className="features-block__text">Nothing to install, fast sign-up and super simple interface. You can setup your office space in a few clicks and let people book it by sending them your company link.</p>
@@ -183,7 +183,7 @@ const IndexPage = () => {
                   </div>
                 </div>
               </div>
-              <div className="row features-block middle-xs center-xs">
+              <div className="row features-block middle-xs center-xs" data-sal="slide-left" data-sal-delay="200" data-sal-easing="ease-out-back">
               <div className="col-xs-12 col-sm-5 col-sm-offset-1">
                   <div className="features-block__image">
                     <img src={AnalyticsImage} />
@@ -194,7 +194,7 @@ const IndexPage = () => {
                   <p className="features-block__text">We digest data for you so you don't have to worry about space optimization. You'll know who's where at any moment and save real rental money by reading our analysis.</p>
                 </div>
               </div>
-              <div className="row features-block middle-xs center-xs">
+              <div className="row features-block middle-xs center-xs" data-sal="slide-right" data-sal-delay="200" data-sal-easing="ease-out-back">
                 <div className="col-xs-12 col-sm-5">
                   <h3 className="features-block__title">Integrations.</h3>
                   <p className="features-block__text">Each company has its stack and we want you to feel you have to adapt. That's why we integrate easily with several solutions such as Slack, Notion or Zapier so you can easily adopt our system.</p>
@@ -222,7 +222,7 @@ const IndexPage = () => {
               <div className="row">
                 {/* Small office */}
                 <div className="col-xs-12 col-sm-4">
-                  <div className="pricing-block">
+                  <div className="pricing-block" data-sal="zoom-in" data-sal-delay="200" data-sal-easing="ease-out-back">
                     <div className="row center-xs">
                       <div className="col-xs-12">
                         <div className="pricing-block__title">
@@ -265,7 +265,7 @@ const IndexPage = () => {
                 </div>
                 {/* Growing office */}
                 <div className="col-xs-12 col-sm-4">
-                  <div className="pricing-block">
+                  <div className="pricing-block" data-sal="zoom-in" data-sal-delay="400" data-sal-easing="ease-out-back">
                     <div className="row center-xs">
                       <div className="col-xs-12">
                         <div className="pricing-block__title">
@@ -308,7 +308,7 @@ const IndexPage = () => {
                 </div>
                 {/* Big office */}
                 <div className="col-xs-12 col-sm-4">
-                  <div className="pricing-block">
+                  <div className="pricing-block" data-sal="zoom-in" data-sal-delay="600" data-sal-easing="ease-out-back">
                     <div className="row center-xs">
                       <div className="col-xs-12">
                         <div className="pricing-block__title">
@@ -364,7 +364,7 @@ const IndexPage = () => {
                   </h2>
                 </div>
               </div>
-              <div className="row features-block middle-xs center-xs">
+              <div className="row features-block middle-xs center-xs" data-sal="slide-right" data-sal-delay="200" data-sal-easing="ease-out-back">
                 <div className="col-xs-12 col-sm-5">
                   <h3 className="features-block__title">The project</h3>
                   <p className="features-block__text">The world is changing and people relation with offices as well. Working partially from home, or going to the office once in a while. Softwares should evolve and be able to manage this properly. Our goal is to be your office space management solution through our simplicity and flexibility.</p>
@@ -375,7 +375,7 @@ const IndexPage = () => {
                   </div>
                 </div>
               </div>
-              <div className="row features-block middle-xs center-xs">
+              <div className="row features-block middle-xs center-xs" data-sal="slide-left" data-sal-delay="200" data-sal-easing="ease-out-back">
               <div className="col-xs-12 col-sm-5">
                   <div className="features-block__image">
                     <img src={TheCompanyImage} />
@@ -390,7 +390,7 @@ const IndexPage = () => {
           </div>
         </div>
         {/* Get started now */}
-        <div className="banner">
+        <div className="banner" data-sal="slide-down" data-sal-delay="400" data-sal-easing="ease-out-back">
           <div className="container-fluid">
             <div className="row center-xs">
               <div className="col-xs-12 col-sm-8">
