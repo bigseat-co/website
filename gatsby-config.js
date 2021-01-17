e97173a7fae052da53726d66bc505998b5cc3516
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-01-17 19:40:07 +0100

    feat: complete removal animations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         'gatsby-plugin-sass',
-        'gatsby-plugin-scroll-reveal',
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
