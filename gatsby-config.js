16f65f9264fa487566b462220159c82f71c74788
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-01-17 15:03:50 +0100

    feat: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
     plugins: [
-        'gatsby-plugin-sass'
+        'gatsby-plugin-sass',
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: "BigSeat",
+              short_name: "BigSeat",
+              start_url: "/",
+              icon: "src/images/icon.png",
+            },
+          },
     ]
 }
